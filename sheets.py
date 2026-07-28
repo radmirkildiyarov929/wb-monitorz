c056a71d821411b4b9660efbf4230820ac2d02cb
radmirkildiyarov929 <radmir.kildiyarov929@gmail.com>
2026-07-28 21:06:19 +0500

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -2,10 +2,10 @@ import os
 import json
 import gspread
 from google.oauth2.service_account import Credentials
+from datetime import datetime
 
 SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
+    "https://www.googleapis.com/auth/spreadsheets"
 ]
 
 credentials = json.loads(os.environ["GOOGLE_CREDENTIALS"])
@@ -17,17 +17,17 @@ creds = Credentials.from_service_account_info(
 
 client = gspread.authorize(creds)
 
-sheet = client.open("WB Monitor").sheet1
+sheet = client.open_by_key(
+    "1TaCX9GEReGMeXEJARbwT8q7-UUD8lFZyPhfpTTB2tPs"
+).sheet1
 
 
 def save_price(article, price):
-    from datetime import datetime
-
     now = datetime.now()
 
     sheet.append_row([
         now.strftime("%d.%m.%Y"),
         now.strftime("%H:%M"),
-        str(article),
+        article,
         price
     ])
